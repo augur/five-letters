75776f16ebefd415d3c8ab72bbd443fd15b37128
Denis Kilchichakov <denis@kilchichakov.com>
2019-07-22 22:24:12 +0300

    Updated Kotlin version, basic Node module

---
+++ five-letters-frontend/build.gradle.kts
@@ -0,0 +1,37 @@
+val gradleNodePluginVersion = "1.3.1"
+
+group = "com.kilchichakov"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven(url = "https://plugins.gradle.org/m2/")
+}
+dependencies {
+    
+    implementation("com.moowork.gradle:gradle-node-plugin:$gradleNodePluginVersion")
+}
+
+plugins {
+    java
+    id("base")
+    id("com.moowork.node") version "1.3.1"
+}
+
+node {
+    // Version of node to use.
+
+    version = "10.14.1"
+
+    // Version of npm to use.
+
+    npmVersion = "6.4.1"
+
+    // If true, it will download node using above parameters.
+
+    // If false, it will try to use globally installed node.
+
+    download = true
+    
+
+}
