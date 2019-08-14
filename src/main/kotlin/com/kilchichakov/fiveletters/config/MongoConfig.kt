b2f98fac25cc6f8e93ac2070bc5bab4f71c96804
Denis Kilchichakov <denis@kilchichakov.com>
2019-08-14 22:08:24 +0300

    Mongo authorization using env variables

--- src/main/kotlin/com/kilchichakov/fiveletters/config/MongoConfig.kt
+++ src/main/kotlin/com/kilchichakov/fiveletters/config/MongoConfig.kt
@@ -1,39 +1,24 @@
 package com.kilchichakov.fiveletters.config
 
-import com.kilchichakov.fiveletters.model.UserData
+import com.mongodb.MongoCredential
+import com.mongodb.ServerAddress
 import com.mongodb.client.MongoDatabase
-import com.mongodb.client.model.IndexOptionDefaults
-import com.mongodb.client.model.IndexOptions
-import org.bson.BSON
-import org.bson.BsonDocument
-import org.bson.BsonValue
-import org.bson.conversions.Bson
 import org.litote.kmongo.KMongo
-import org.litote.kmongo.getCollection
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import javax.annotation.PostConstruct
 
 
 @Configuration
 class MongoConfig {
 
     @Bean
-    fun mongoDatabase(): MongoDatabase {
-        val client = KMongo.createClient("localhost", 27018)
-        return client.getDatabase("five-letters") //normal java driver usage
+    fun mongoDatabase(@Value("\${MONGO_LOGIN}") login: String,
+                      @Value("\${MONGO_PASSWORD}") password: String,
+                      @Value("\${MONGO_HOST}") host: String,
+                      @Value("\${MONGO_PORT}") port: Int): MongoDatabase {
+        val cred = MongoCredential.createCredential(login, "admin", password.toCharArray())
+        val client = KMongo.createClient(ServerAddress(host, port), listOf(cred))
+        return client.getDatabase("five-letters")
     }
-
-    @PostConstruct
-    fun mongoInitialSetup() {
-        if (true) return
-        val db = mongoDatabase()
-        val key = BsonDocument.parse("""{ "login": 1 }""")
-        val indexOptions = IndexOptions().unique(true)
-        db.getCollection<UserData>().createIndex(key, indexOptions)
-    }
-
-    private val INITIAL_MONGO_SETUP = """
-        db.userData.createIndex( { "login": 1 }, { unique: true } )
-    """.trimIndent()
 }
