3ec5f39d06957b3421bf1e4044fd8a52c9c089b5
Denis Kilchichakov <denis@kilchichakov.com>
2019-08-18 21:10:14 +0300

    jwt now has expiration time

--- src/main/kotlin/com/kilchichakov/fiveletters/service/JwtService.kt
+++ src/main/kotlin/com/kilchichakov/fiveletters/service/JwtService.kt
@@ -6,24 +6,30 @@ import com.kilchichakov.fiveletters.model.UserData
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Service
+import java.util.Calendar
 
 
 @Service
 class JwtService(
-        @Value("\${jwt.secret}") secret: String,
-        @Value("\${jwt.issuer}") private val issuer: String
+        @Value("\${JWT_SECRET}") secret: String,
+        @Value("\${JWT_ISSUER}") private val issuer: String,
+        @Value("\${JWT_TTL}") private val ttlSeconds: Int
 ) {
 
     private val algorithm = Algorithm.HMAC256(secret)
     private val verifier = JWT.require(algorithm)
             .withIssuer(issuer)
+            .acceptLeeway(1)
             .build()
 
 
     fun generateToken(userDetails: UserDetails): String {
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.SECOND, ttlSeconds)
         return JWT.create()
                 .withIssuer(issuer)
                 .withSubject(userDetails.username)
+                .withExpiresAt(calendar.time)
                 .sign(algorithm)
     }
 
