eb4d553e44f87a785e6942e5a14783c78c698d8a
Denis Kilchichakov <denis@kilchichakov.com>
2019-10-08 20:56:56 +0300

    cors configured, version 0.1.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ plugins {
 }
 
 group = "com.kilchichakov"
-version = "0.1.1-SNAPSHOT"
+version = "0.1.2-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
--- src/main/kotlin/com/kilchichakov/fiveletters/config/SecurityConfig.kt
+++ src/main/kotlin/com/kilchichakov/fiveletters/config/SecurityConfig.kt
@@ -16,6 +16,9 @@ import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 
 @Configuration
@@ -40,10 +43,19 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
         return BCryptPasswordEncoder()
     }
 
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration().applyPermitDefaultValues()
+        source.registerCorsConfiguration("/**", config)
+        return source
+    }
+
     override fun configure(security: HttpSecurity) {
         security
                 .httpBasic().disable()
-                .cors().and().csrf().disable()
+                .cors()
+                .and().csrf().disable()
                 .authorizeRequests().antMatchers("/auth", "/register").permitAll()
                 .anyRequest().authenticated()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
