8fa2214b562a173f2c13bcfeb098061f832de369
Denis Kilchichakov <denis@kilchichakov.com>
2019-11-03 11:26:36 +0300

    More time-periods

--- src/main/resources/mongo-data-setup.js
+++ src/main/resources/mongo-data-setup.js
@@ -40,3 +40,53 @@ db.timePeriod.save(
          enabled: true
      }
 )
+db.timePeriod.save(
+     {
+         _id: "MONTH",
+         days: 0,
+         weeks: 0,
+         months: 1,
+         years: 0,
+         enabled: true
+     }
+)
+db.timePeriod.save(
+     {
+         _id: "TWO_MONTHS",
+         days: 0,
+         weeks: 0,
+         months: 2,
+         years: 0,
+         enabled: true
+     }
+)
+db.timePeriod.save(
+     {
+         _id: "FOUR_MONTHS",
+         days: 0,
+         weeks: 0,
+         months: 4,
+         years: 0,
+         enabled: true
+     }
+)
+db.timePeriod.save(
+     {
+         _id: "SIX_MONTHS",
+         days: 0,
+         weeks: 0,
+         months: 6,
+         years: 0,
+         enabled: true
+     }
+)
+db.timePeriod.save(
+     {
+         _id: "YEAR",
+         days: 0,
+         weeks: 0,
+         months: 0,
+         years: 1,
+         enabled: true
+     }
+)
