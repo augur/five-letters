f5f16baa40f55a1735a80da47b44c7f7fceaf084
Denis Kilchichakov <denis@kilchichakov.com>
2020-04-30 20:29:56 +0300

    transaction aware extension methods on collection

--- src/main/kotlin/com/kilchichakov/fiveletters/service/TransactionWrapper.kt
+++ src/main/kotlin/com/kilchichakov/fiveletters/service/TransactionWrapper.kt
@@ -1,9 +1,16 @@
 package com.kilchichakov.fiveletters.service
 
+import com.kilchichakov.fiveletters.exception.DatabaseException
 import com.mongodb.MongoClient
 import com.mongodb.client.ClientSession
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.result.UpdateResult
+import org.bson.conversions.Bson
+import org.litote.kmongo.findOne
 import org.springframework.stereotype.Service
 import java.lang.Exception
+
+private val threadLocalSession = ThreadLocal<ClientSession>()
 
 @Service
 class TransactionWrapper(
@@ -12,6 +19,7 @@ class TransactionWrapper(
 
     fun executeInTransaction(block:(session: ClientSession) -> Unit) {
         client.startSession().use {
+            threadLocalSession.set(it)
             try {
                 it.startTransaction()
                 block(it)
@@ -19,7 +27,29 @@ class TransactionWrapper(
             } catch (e: Exception) {
                 it.abortTransaction()
                 throw e
+            } finally {
+                threadLocalSession.remove()
             }
         }
+    }
+}
+
+fun MongoCollection<*>.updateOneInTransaction(filter: Bson, update: Bson, mandatory: Boolean): UpdateResult {
+    val session = threadLocalSession.get()
+    return if (session == null) {
+        if (mandatory) throw DatabaseException("Attempted to run updateOne filter=$filter, update=$update w/o transaction")
+        this.updateOne(filter, update)
+    } else {
+        this.updateOne(session, filter, update)
+    }
+}
+
+fun <T>MongoCollection<T>.findOneInTransaction(filter: Bson, mandatory: Boolean): T? {
+    val session = threadLocalSession.get()
+    return if (session == null) {
+        if (mandatory) throw DatabaseException("Attempted to run findOne filter=$filter, w/o transaction")
+        this.findOne(filter)
+    } else {
+        this.findOne(session, filter)
     }
 }
--- src/test/unit/kotlin/com/kilchichakov/fiveletters/service/TransactionWrapperTest.kt
+++ src/test/unit/kotlin/com/kilchichakov/fiveletters/service/TransactionWrapperTest.kt
@@ -1,7 +1,9 @@
 package com.kilchichakov.fiveletters.service
 
+import com.kilchichakov.fiveletters.exception.DatabaseException
 import com.mongodb.MongoClient
 import com.mongodb.client.ClientSession
+import com.mongodb.client.MongoCollection
 import io.mockk.confirmVerified
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
@@ -9,13 +11,18 @@ import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThatCode
+import org.bson.conversions.Bson
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.provider.Arguments
+import org.litote.kmongo.findOne
 
 @ExtendWith(MockKExtension::class)
 internal class TransactionWrapperTest {
-
 
     @RelaxedMockK
     lateinit var client: MongoClient
@@ -69,4 +76,80 @@ internal class TransactionWrapperTest {
 
         confirmVerified(client, session, block)
     }
+
+    @Nested
+    inner class TransactionalMethodsTest {
+
+        lateinit var session: ClientSession
+        lateinit var collection: MongoCollection<Int>
+        lateinit var filter: Bson
+        lateinit var update: Bson
+
+        @BeforeEach
+        fun setUp() {
+            session = mockk(relaxed = true)
+            collection = mockk(relaxed = true)
+            filter = mockk()
+            update = mockk()
+            every { client.startSession() } returns session
+        }
+
+        fun methods() = listOf(
+                Args(
+                        { mandatory -> collection.updateOneInTransaction(filter, update, mandatory) },
+                        { collection.updateOne(session, filter, update) },
+                        { collection.updateOne(filter, update) }
+                ),
+                Args(
+                        { mandatory -> collection.findOneInTransaction(filter, mandatory) },
+                        { collection.findOne(session, filter) },
+                        { collection.findOne(filter) }
+                )
+        )
+
+        @Test
+        fun `should run method in transaction`() {
+            methods().forEach { args ->
+                // When
+                wrapper.executeInTransaction {
+                    args.methodCall(true)
+                }
+
+                // Then
+                verify { args.collectionCall() }
+                confirmVerified(collection)
+            }
+        }
+
+        @Test
+        fun `should fail to run if transaction is mandatory and not exist`() {
+            methods().forEach { args ->
+                // When
+                assertThatCode {
+                    args.methodCall(true)
+                }.isInstanceOf(DatabaseException::class.java)
+
+                // Then
+                confirmVerified(collection)
+            }
+        }
+
+        @Test
+        fun `should not fail if transaction is not mandatory and not exist`() {
+            methods().forEach { args ->
+                // When
+                assertThatCode {
+                    args.methodCall(false)
+                }.doesNotThrowAnyException()
+
+                // Then
+                verify { args.collectionCallNoSession() }
+                confirmVerified(collection)
+            }
+        }
+    }
+
+    data class Args(val methodCall: (Boolean) -> Unit,
+                    val collectionCall: () -> Unit,
+                    val collectionCallNoSession: () -> Unit)
 }
