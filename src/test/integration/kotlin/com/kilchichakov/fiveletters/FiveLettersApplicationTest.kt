8031b147a8f818e2a514aee673ca0d2fdc3d7458
Denis Kilchichakov <denis@kilchichakov.com>
2020-05-03 15:23:46 +0300

    mockked monog in integration test

--- src/test/integration/kotlin/com/kilchichakov/fiveletters/FiveLettersApplicationTest.kt
+++ src/test/integration/kotlin/com/kilchichakov/fiveletters/FiveLettersApplicationTest.kt
@@ -1,9 +1,7 @@
 package com.kilchichakov.fiveletters
 
-import io.mockk.clearAllMocks
-import io.mockk.unmockkAll
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
+import com.mongodb.client.MongoDatabase
+import com.ninjasquad.springmockk.MockkBean
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
@@ -18,6 +16,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class FiveLettersApplicationTest {
+
+	@MockkBean(relaxed = true)
+	lateinit var mongoDatabase: MongoDatabase
 
 	@Test
 	fun contextLoads() {
